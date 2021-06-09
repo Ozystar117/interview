7d161ff73164aa3fe019ebd1e8251365001578c6
Emmanuel Ozioma <Emmanuel Ozioma@OziomaJr>
2021-06-09 02:38:11 +0100

    initial commit

---
+++ interview/interview/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package interview;
+
+/**
+ * @author Emmanuel Ozioma
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
